1fb2b04243106819c6b207684256ecec4a125fb2
manishamaheswari <ques06541@adobe.com>
2024-10-18 12:36:07 +0530

    implemented pagination

--- blocks/countries/countries.js
+++ blocks/countries/countries.js
@@ -1,28 +1,92 @@
 import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
 let sheetdata = null;
+let currentPage = 1;
+const itemsPerPage = 20;
+
+// Function to render pagination controls
+function renderPaginationControls(totalItems) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const paginationContainer = document.createElement('div');
+  paginationContainer.classList.add('pagination-controls');
+
+  // Previous button
+  const prevBtn = document.createElement('button');
+  prevBtn.innerText = 'Previous';
+  prevBtn.disabled = currentPage === 1; // Disable if on the first page
+  prevBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      updateTable();
+    }
+  });
+  paginationContainer.append(prevBtn);
+
+  // Page indicator
+  const pageIndicator = document.createElement('span');
+  pageIndicator.innerText = `Page ${currentPage} of ${totalPages}`;
+  paginationContainer.append(pageIndicator);
+
+  // Next button
+  const nextBtn = document.createElement('button');
+  nextBtn.innerText = 'Next';
+  nextBtn.disabled = currentPage === totalPages; // Disable if on the last page
+  nextBtn.addEventListener('click', () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      updateTable();
+    }
+  });
+  paginationContainer.append(nextBtn);
+
+  return paginationContainer;
+}
+
+// Function to update the table with pagination
+async function updateTable() {
+  const tableContainer = document.querySelector('.countries-block');
+  // const tableContainer = document.getElementsByClassName('countries-block');
+  console.log(tableContainer)
+  tableContainer.innerHTML = ''; // Clear the current table and pagination
+
+  // Recreate the table with sliced data
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedData = sheetdata.slice(start, end);
+
+  // Create new table with paginated data
+  const table = document.createElement('table');
+  await createTableHeader(table); // Your existing createTableHeader function
+  paginatedData.forEach((row, i) => {
+    createTableRow(table, row, start + i + 1); // Your existing createTableRow function
+  });
+  
+  // Append the table and pagination controls
+  tableContainer.append(table);
+  const paginationControls = renderPaginationControls(sheetdata.length);
+  tableContainer.append(paginationControls);
+}
+
+// Initialization function that fetches the data and sets up the table
+async function initTableWithPagination(jsonURL) {
+  const resp = await fetch(jsonURL);
+  const json = await resp.json();
+  sheetdata = json.data;
+  console.log(sheetdata)
+
+  // Initially load the first page
+  updateTable();
+}
 
 async function createTableHeader(table){
   let tr=document.createElement("tr");
-  let tableHeadings = Object.keys(sheetdata);
+  console.log(sheetdata)
+  let tableHeadings = Object.keys(sheetdata[0]);
   console.log(tableHeadings)
   tableHeadings.map((ele,i) => {
     let eleName = document.createElement("th");
     eleName.appendChild(document.createTextNode(ele));
     tr.append(eleName)
   })
-    // let conuntry=document.createElement("th");
-    // conuntry.appendChild(document.createTextNode(country));
-    // let continenth=document.createElement("th");
-    // continenth.appendChild(document.createTextNode(continent));
-    // let capitalh=document.createElement("th");
-    // capitalh.appendChild(document.createTextNode(capital));
-    // let abbr=document.createElement("th");
-    // abbr.appendChild(document.createTextNode(abbreviation));
-    // tr.append(sno);
-    // tr.append(conuntry);
-    // tr.append(capitalh);
-    // tr.append(continenth);
-    // tr.append(abbr);
     table.append(tr);
 }
 async function createTableRow(table,row,i){
@@ -47,37 +111,16 @@ async function createTableRow(table,row,i){
     table.append(tr);
 }
 
-// async function createSelectMap(jsonURL){
-//     const optionsMap=new Map();
-//     const { pathname } = new URL(jsonURL);
-
-//     const resp = await fetch(pathname);
-//     optionsMap.set("all",allCountries);optionsMap.set("asia",asia);optionsMap.set("europe",europe);optionsMap.set("africa",africa);optionsMap.set("america",america);optionsMap.set("australia",australia);
-//     const select=document.createElement('select');
-//     select.id = "region";
-//     select.name="region";
-//     optionsMap.forEach((val,key) => {
-//         const option = document.createElement('option');
-//         option.textContent = val;
-//         option.value = key;
-//         select.append(option);
-//       });
-     
-//      const div=document.createElement('div'); 
-//      div.classList.add("region-select");
-//      div.append(select);
-//     return div;
-// }
 async function createTable(jsonURL,val) {
 
-    let  pathname = null;
-    if(val){
-        pathname=jsonURL;
-    }else{
-        pathname= new URL(jsonURL);
-    }
+    // let  pathname = null;
+    // if(val){
+    //     pathname=jsonURL;
+    // }else{
+    //     pathname= new URL(jsonURL);
+    // }
     
-    const resp = await fetch(pathname);
+    const resp = await fetch(jsonURL);
     const json = await resp.json();
     console.log("=====JSON=====> {} ",json);
     sheetdata = json.data[0]
@@ -85,12 +128,9 @@ async function createTable(jsonURL,val) {
     const table = document.createElement('table');
     createTableHeader(table);
     json.data.forEach((row,i) => {
-
-        createTableRow(table,row,(i+1));
-
-      
+      createTableRow(table,row,(i+1));
     });
-    
+
     return table;
 }    
 
@@ -99,25 +139,20 @@ export default async function decorate(block) {
     const countries = block.querySelector('a[href$=".json"]');
     console.log(countries)
     const parientDiv=document.createElement('div');
-    parientDiv.classList.add('contries-block');
+    parientDiv.classList.add('countries-block');
+    // console.log(document.querySelector('contries-block'));
+    console.log(parientDiv);
     // console.log(countries.href)
 
     if (countries) {
+      block.appendChild(parientDiv);
         // parientDiv.append(await createSelectMap(countries.href));
-        parientDiv.append(await createTable(countries.href,null));
+        // parientDiv.append(await createTable(countries.href,null));
+        // countries.replaceWith(parientDiv);
+
+        console.log(countries.href)
+        await initTableWithPagination(countries.href);
         countries.replaceWith(parientDiv);
         
     }
-    // const dropdown=document.getElementById('region');
-    //   dropdown.addEventListener('change', () => {
-    //     let url=countries.href;
-    //     if(dropdown.value!='all'){
-    //         url=countries.href+"?sheet="+dropdown.value;
-    //     }
-    //     const tableE=parientDiv.querySelector(":scope > table");
-    //     let promise = Promise.resolve(createTable(url,dropdown.value));
-    //     promise.then(function (val) {
-    //         tableE.replaceWith(val);
-    //     });
-    //   });
   }
